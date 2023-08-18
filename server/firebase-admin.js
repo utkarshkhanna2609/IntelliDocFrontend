096bfc1cf6dd20b7de476635526a497919381133
Utkarsh Khanna <107148451+utkarshkhanna2609@users.noreply.github.com>
2023-08-18 16:44:19 +0530

    Update firebase-admin.js

--- server/firebase-admin.js
+++ server/firebase-admin.js
@@ -12,7 +12,7 @@ const serviceAccount = {
   type: process.env.GC_TYPE,
   project_id: process.env.GC_PROJECT_ID,
   private_key_id: process.env.GC_PRIVATE_KEY_ID,
-  private_key: process.env.GC_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  private_key: process.env.GC_PRIVATE_KEY.replace,
   client_email: process.env.GC_CLIENT_EMAIL,
   client_id: process.env.GC_CLIENT_ID,
   auth_uri: process.env.GC_AUTH_URI,
