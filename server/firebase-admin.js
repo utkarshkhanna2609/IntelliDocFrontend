ac6d127ec3cc731e84f4e431706e7105725a1fd5
Utkarsh Khanna <107148451+utkarshkhanna2609@users.noreply.github.com>
2023-08-19 01:00:10 +0530

    firebase fix

--- server/firebase-admin.js
+++ server/firebase-admin.js
@@ -1,15 +1,36 @@
 
 
-import admin from 'firebase-admin';
-import serviceAccount from '../firebaseServiceAccount.json'; // Path to your service account key JSON file
-
+import admin from 'firebase-admin'; // Path to your service account key JSON file
+import dotenv from 'dotenv';
 let firebaseApp;
+
+
+
+
+dotenv.config();
+const serviceAccount = {
+  type: process.env.GC_TYPE,
+  project_id: process.env.GC_PROJECT_ID,
+  private_key_id: process.env.GC_PRIVATE_KEY_ID,
+  private_key: process.env.GC_PRIVATE_KEY,
+  client_email: process.env.GC_CLIENT_EMAIL,
+  client_id: process.env.GC_CLIENT_ID,
+  auth_uri: process.env.GC_AUTH_URI,
+  token_uri: process.env.GC_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.GC_AUTH_PROVIDER_CERT_URL,
+  client_x509_cert_url: process.env.GC_CLIENT_CERT_URL,
+};
+
+
+
+
 
 try {
   console.log("firebase-manager admin get app");
+  console.log(serviceAccount);
   firebaseApp = admin.app(); // Attempt to retrieve the default app instance
 } catch (error) {
-  console.log("firebase-manager admin get app--- app not avaliable so initialize it");
+  console.log("firebase-manager admin get app--- app not avaliable so initialize it", error);
 
   firebaseApp = admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
